0af54f0c4ff26c95fbe4fd4ee6f865195c0cc69b
tulio1057 <tuliojfb283@gmail.com>
2025-12-30 21:34:35 -0300

    Correção na logo

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
   },
   envDir: path.resolve(import.meta.dirname),
   root: path.resolve(import.meta.dirname, "client"),
+  publicDir: "public",
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
